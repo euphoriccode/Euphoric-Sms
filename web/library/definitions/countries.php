44fe9a7786c93f2ed017b19da453fe7e258072db
Matthew Watts <mwatts1212@gmail.com>
2011-07-08 00:26:59 +0100

    Added Country Arrays

---
+++ web/library/definitions/countries.php
@@ -0,0 +1,52 @@
+<?php
+
+$countries = array( 'au' => array(
+'name' => 'Australia',
+'countryCode' => '61',
+'trunkPrefix' => '0',
+'numberLength' => '9'
+),
+
+
+'br' => array(
+'name' => 'Brazil',
+'countryCode' => '55',
+'trunkPrefix' => '0',
+'numberLength' => '10'
+ ),
+ 
+ 
+'ca' => array(
+'name' => 'Canada',
+'countryCode' => '1',
+'trunkPrefix' => '1',
+'numberLength' => '10'
+),
+
+
+'fr' => array(
+'name' => 'France',
+'countryCode' => '33',
+'trunkPrefix' => '0',
+'numberLength' => '9'
+),
+        
+		
+'uk' => array(
+'name' => 'United Kingdom',
+'countryCode' => '44',
+'trunkPrefix' => '0',
+'numberLength' => '10'
+),
+        
+		
+'us' => array(
+'name' => 'United States',
+'countryCode' => '1',
+'trunkPrefix' => '1',
+'numberLength' => '10'
+)
+
+);
+
+?>
